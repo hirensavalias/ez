154e88fa9daecde487391bcf5ca6f82449e2527a
hirensavalia <hiren.savalias@gmail.com>
2020-12-22 15:16:23 +0530

    initial

---
+++ components/Map.js
@@ -0,0 +1,39 @@
+import React from "react";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    position: "relative",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export const MapMarker = () => (
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}
+    >
+      <Marker
+        coordinate={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+        }}
+      ></Marker>
+    </MapView>
+  </View>
+);
